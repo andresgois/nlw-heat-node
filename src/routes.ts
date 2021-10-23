204c9a236b2f7c61ccacc32531265058a61012ad
andre gois <andre.s.gois3@gmail.com>
2021-10-23 16:03:52 -0300

    adding the get last and profile routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controller/AuthenticateUserController";
 import { CreateMessageControler } from "./controller/CreateMessageControler";
+import { GetLast3MessagesController } from "./controller/GetLast3MessagesController";
+import { ProfileUserController } from "./controller/ProfileUserController";
 import { ensureAuthenticate } from "./middleware/ensureAuthenticate";
 
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/authenticate", new AuthenticateUserController().handle);
 
-router.post("/messages", ensureAuthenticate, new CreateMessageControler().handle)
+router.post("/messages", ensureAuthenticate, new CreateMessageControler().handle);
+
+router.get("/messages/getLast3", new GetLast3MessagesController().handle);
+
+router.get("/profile", ensureAuthenticate, new ProfileUserController().handle);
 
 export { router };
